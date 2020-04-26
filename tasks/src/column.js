f9291135cb435294d84dd72fc4ad9a200a64e82b
Elton C. Rego <rego.elton@gmail.com>
2020-04-26 16:27:12 -0700

    Set up initial data

---
+++ tasks/src/column.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Column = ({ column, tasks }) => (
+  column.title
+);
+
+export default Column;
--- tasks/src/index.js
+++ tasks/src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import data from './data';
+import Column from './column';
 
-const App = () => 'Hello world!';
+const App = () => {
+  const [taskData, setTaskData] = useState(data);
+
+  return taskData.columnOrder.map((columnId) => {
+    const column = taskData.columns[columnId];
+    const tasks = column.taskIds.map(taskId => taskData.tasks[taskId]);
+    return <Column key={column.id} column={column} tasks={tasks}/>;
+  });
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
