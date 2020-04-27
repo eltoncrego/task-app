d044609efc57865f3ecf237b9b680fb644116fb4
Elton C. Rego <rego.elton@gmail.com>
2020-04-26 17:08:10 -0700

    Preserve reorder operations with state

--- tasks/src/index.js
+++ tasks/src/index.js
@@ -8,7 +8,38 @@ import Column from "./column";
 const App = () => {
   const [taskData, setTaskData] = useState(data);
 
-  const onDragEnd = (result) => {};
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = taskData.columns[source.droppableId];
+    const newTaskIds = column.taskIds.slice();
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newTaskData = {
+      ...taskData,
+      columns: {
+        ...taskData.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+    setTaskData(newTaskData);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
